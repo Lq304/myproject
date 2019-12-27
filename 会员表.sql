7f7835917757a41c80200cfc72bf616d284b7d4b
lq <3289369217@qq.com>
2019-12-27 20:47:39 +0800

    上传聚划算原码

---
+++ 会员表.sql
@@ -0,0 +1,18 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2019-10-18 21:23:49
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "vip"
+#
+
+DROP TABLE IF EXISTS `vip`;
+CREATE TABLE `vip` (
+  `username` varchar(20) NOT NULL,
+  `userpass` varchar(16) NOT NULL,
+  `sex` char(2) DEFAULT '女',
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ 商品表和购物车表.sql
@@ -0,0 +1,47 @@
+﻿--会员表
+
+CREATE TABLE `vip` (
+  `username` varchar(20) NOT NULL,
+  `userpass` varchar(16) NOT NULL,
+  `sex` char(2) DEFAULT '女',
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+select * from vip
+
+--商品表
+create table goodsInfo
+(
+       goodsId varchar(10) primary key,
+       goodsName varchar(100),
+       goodsType varchar(20),
+       goodsPrice float,
+       goodsCount int,
+       goodsDesc varchar(100),
+       goodsImg varchar(100),
+       beiyong1  varchar(100),
+       beiyong2  varchar(100),
+       beiyong3  varchar(100),
+       beiyong4  varchar(100),
+       beiyong5  varchar(100),
+       beiyong6  varchar(100),
+       beiyong7  varchar(100),
+       beiyong8  varchar(100),
+       beiyong9  varchar(100),
+       beiyong10  varchar(100),
+       beiyong11  varchar(100),
+       beiyong12  varchar(100),
+       beiyong13  varchar(100)
+);
+
+--购物车表
+create table shoppingCart(
+       vipName varchar(10),
+       goodsId varchar(10),
+       goodsCount int 
+);
+
+
+
+select * from shoppingCart
